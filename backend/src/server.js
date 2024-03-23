b097f1e2844d4a1fb557cb03ff2d99c14ffc6374
Lucas Jandrey <wdev.lucas@gmail.com>
2024-03-23 01:35:44 -0300

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.post('/add_to_queue', (request, response) => {
+    
+})
+
+app.listen(3000, () => {
+    console.log('-- Backend is running --')
+})
